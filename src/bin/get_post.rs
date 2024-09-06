3a13c417dbdf3fb5aec9e0ba2d7a87d9aaa53dc0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-06 19:54:37 +0100

    Update get_post.rs

--- src/bin/get_post.rs
+++ src/bin/get_post.rs
@@ -1,7 +1,7 @@
 use poc_api_client::client::RestApiClient;
 use poc_api_client::models::Post;
 
-fn main(){
+async fn main(){
     let base_url = "https://jsonplaceholder.typicode.com";
     let client = RestApiClient::new(base_url);
 
