9240715cef378aa00d1c06692c948dcc7e0f30df
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-06 19:56:13 +0100

    Update get_post.rs

--- src/bin/get_post.rs
+++ src/bin/get_post.rs
@@ -1,11 +1,11 @@
 use poc_api_client::client::RestApiClient;
 use poc_api_client::models::Post;
 
-async fn main(){
+fn main(){
     let base_url = "https://jsonplaceholder.typicode.com";
     let client = RestApiClient::new(base_url);
 
     // GET Request
-    let post = client.get_post(1).await.unwrap();
+    let post = client.get_post(1).unwrap();
     println!("Fetched Post: {:?}", post);
 }
